eded405a0c601d53215e3300ee346f9c8e94c22b
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2024-08-10 12:21:42 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,28 @@
+let score="33"//this show the string data type
+
+// console.log(typeof score);
+//now we change the string data type into number data type
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber); //this is how we cange the string data type into number data type
+
+// "33"=>33
+// "33aabc"=>NaN
+
+
+
+// IN BOOLEAN CONVERSION 
+// true=>1;false=>0
+//""=>false emptry string
+// "himanshu"=>true string with value always show the true in boolean
+
+
+let isLoggedIn=1
+let boolConVert=Boolean(isLoggedIn);//BY THIS WE CHANGE THE NUMBER VALUE IN BOOLEAN 
+// console.log(boolConVert)
+
+// conversion into string 
+let Alpha=65
+let stringConvert=String(Alpha);
+console.log(stringConvert);
+console.log(typeof stringConvert);//this show this is now string 
