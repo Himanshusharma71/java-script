344808a86caeeb80550f3ea2b771da64bcbb13ec
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2024-08-10 12:53:45 +0000

    operation and comparision

---
+++ basics/operations.js
@@ -0,0 +1,42 @@
+console.log("operation")
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)//show tha power 
+// console.log(2/2)
+// console.log(2%2)//show the remainder
+
+// let str1="himanshu "
+// let str2="sharma"
+// let str3=str1+str2
+// console.log("the full name is ",str3)
+
+// console.log("1"+2);//here it merge a string with the number
+// console.log(1+"2");//here it merge string and number
+// console.log("1"+2+2);//here it work as a string
+// console.log(1+2+"2");//if the string is in the begining of the statement then it work as a string else firt it perform the operations 
+
+
+// comparision datatype
+//WHEN EVER YOU WANT TO COMPAIR MAKE SURE DATA TYPE SHOULD BE SAME 
+
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2<=1) 
+
+// console.log("2"> 1) //true here js auto convert "2" in number so it show true
+//THE compair WHICH CREATE PROBLEMS
+
+console.log(null>0)
+console.log(null<0)
+console.log(null==0)
+console.log(null>=0)//only this show true 
+
+// strict check
+
+// if you used === then this will not only check the value also check datatype
