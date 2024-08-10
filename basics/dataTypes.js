f03bb58538df503630e188e758a6a2238ec059f7
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2024-08-10 11:45:09 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use scrict"//By the "use strict" your code will treated as a newer version of javascript4
+
+let name="himanshu"//string data type 
+let age=23 //number data type
+let isLoggedIn= true //boolean data type 
+
+// Data types in javascript
+
+// number=> 2 to power 53
+// BigInt
+// String
+// Boolean
+// null =>standalone value
+// undefined
+//symbol =>unique 
+
+//object
+//array 
