e695492f2dac3f9b9661d92b62f38998c9b5e0e0
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2024-10-02 01:27:45 +0530

    Add files via upload

---
+++ basics/num_and_math.js
@@ -0,0 +1,30 @@
+// const score =400;
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);//coinvert into the string and then give the length
+// console.log(balance.toFixed(2));//here it show only 2 digit value after the decimal like 100.00
+
+
+// const otherNum=23.65456;
+// console.log(otherNum.toFixed(2));
+// console.log(otherNum.toPrecision(2));
+
+// const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-In')); //this show the comas after the 3 digit that we use while writing the number 
+
+// //===========maths===================
+
+console.log(Math);
+// console.log(Math.abs(-4));//this gives us absulute value
+// console.log(Math.round(5.6));//this gives us the rouund of value 6 
+// console.log(Math.ceil(5.3));//also give the round of value but the value is 6 not 5 take greater number 
+// console.log(Math.floor(5.9));//this will also gives the roundof value but the take smaller value 5 not 6
+// console.log(Math.min(2,3,5,4,9));//give smallest value 
+// console.log(Math.max(2,4,2,5,4,6,8));//gives max number value
+// console.log(Math.pow(2,4));//give the 2 power 4 16
+// console.log(Math.sqrt(16)); // Outputs: 4
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min); //this gives the random value in between 10 to 20 
