67b035039b2105e12bb28d7dedf4048eb5a3fd10
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2024-10-02 02:21:11 +0530

    Add files via upload

---
+++ basics/dates.js
@@ -0,0 +1,21 @@
+// let myDate= new Date();
+// console.log(myDate);//2024-10-01T20:25:23.923Z
+// console.log((myDate).toString());//Wed Oct 02 2024 01:55:23 GMT+0530 (India Standard Time)
+// console.log((myDate).toDateString());//Wed Oct 02 2024
+// console.log((myDate).toLocaleString());//2/10/2024, 1:55:23 am
+// console.log((myDate).toJSON());//2024-10-01T20:25:23.923Z
+// console.log(typeof myDate);//object 
+
+// let myCreatedDate=new Date(2024,0,28)//months started in js from 0
+// console.log(myCreatedDate.toDateString());//Sun Jan 28 2024
+// console.log(myCreatedDate.toLocaleString()); //28/1/2024, 12:00:00 am
+// const newCreatedDate= new Date("01-14-2023");//this is MM-DD-YY format show of date 
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp.toString());
+// console.log(Date.now()/1000);
+
+let newDate= new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate);
